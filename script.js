f4e7a178af6328a8c9cd4fa29192dbf39d1be837
Emmyfx <ayomidedairo194@gmail.com>
2024-11-22 15:46:19 +0100

    fixed the navbar

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-document.querySelector(".toggle-btn").addEventListener("click", () => {
-    document.querySelector(".nav-links").style.display="flex"
-})
+// document.querySelector(".toggle-btn").addEventListener("click", () => {
+//     document.querySelector(".nav-links").style.display="flex"
+// })
+
+// let menuList = document.getElementById("menuList")
+//     menuList.style.maxHeight = "0px";
+// function toggleMenu() {
+//     if (menuList.style.maxHeight == "0px") 
+//         {
+//         menuList.style.maxHeight = "300px";
+//     }
+//     else {
+//         menuList.style.maxHeight = "0px";
+//     }
+// }
+
+const sidebar = document.querySelector(".sidebar")
+    nav-btn.addeventlistener
