fe06709fddebcd8affa3440bd33181d72c0fcb94
Emmyfx <ayomidedairo194@gmail.com>
2024-11-24 14:20:02 +0100

    Toggle-btn modified

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-// document.querySelector(".toggle-btn").addEventListener("click", () => {
-//     document.querySelector(".nav-links").style.display="flex"
-// })
-
-// let menuList = document.getElementById("menuList")
-//     menuList.style.maxHeight = "0px";
-// function toggleMenu() {
-//     if (menuList.style.maxHeight == "0px") 
-//         {
-//         menuList.style.maxHeight = "300px";
-//     }
-//     else {
-//         menuList.style.maxHeight = "0px";
-//     }
-// }
-
-const sidebar = document.querySelector(".sidebar")
-    nav-btn.addeventlistener
+function showSidebar() {
+    const menuList = document.getElementById("menuList");
+    menuList.classList.toggle("active");
+}
